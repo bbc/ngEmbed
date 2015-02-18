13f378a71008c08d4f0de1c3fe5f39743bcb5644
suggim01 <mike.suggitt@bbc.co.uk>
2015-02-18 17:39:47 +0000

    - Changed directive to use ngmodel

--- app/ngEmbed.js
+++ app/ngEmbed.js
@@ -1,24 +1,21 @@
-
-
-
-
 function ngEmbedController($scope, oEmbedProviderService, longifyService) {
+    $scope.internalModel = {};
     $scope.embedUrl = '';
     $scope.embedHTML = '';
 
-    $scope.$watch('embedUrl', function(newValue) {
-        if(newValue) {
-            longifyService.longify(newValue).then(function(longUrl) {
+    $scope.$watch('embedUrl', function (newValue) {
+        if (newValue) {
+            $scope.internalModel.originalUrl = newValue;
+            longifyService.longify(newValue).then(function (longUrl) {
                 var provider = oEmbedProviderService.getOEmbedProvider(longUrl);
                 provider.params = {};
-                oEmbedProviderService.getEmbedHTML(longUrl, provider).then(function(html) {
+                oEmbedProviderService.getEmbedHTML(longUrl, provider).then(function (html) {
                     $scope.embedHTML = html;
+                    $scope.internalModel.longUrl = longUrl;
+                    $scope.internalModel.provider = provider;
+                    $scope.internalModel.html = html;
                 });
             });
-
-
-
-
         }
     });
 
@@ -28,14 +25,21 @@ function ngEmbed() {
     return {
         scope: {},
         restrict: 'EA',
+        require: '?ngModel',
         controller: 'ngEmbedController',
-        link: function(scope, element, attr) {
-            attr.$observe('embedUrl', function(value) {
+        link: function (scope, element, attr, ngModel) {
+            attr.$observe('embedUrl', function (value) {
                 scope.embedUrl = value;
             });
-            scope.$watch('embedHTML', function(newValue) {
-                if(newValue) {
+            scope.$watch('embedHTML', function (newValue) {
+                if (newValue) {
                     element.html(newValue);
+                }
+            });
+
+            scope.$watch('internalModel', function (newValue) {
+                if (newValue && ngModel) {
+                    ngModel.$setViewValue(newValue);
                 }
             });
         }
--- test/unit/ngEmbed.spec.js
+++ test/unit/ngEmbed.spec.js
@@ -1,28 +1,30 @@
 
 
 describe('ngEmbed', function() {
-    var scope, $rootScope, $httpBackend, element, $compile;
+    var scope, $rootScope, model, $httpBackend, element, $compile;
 
     beforeEach(module('ngEmbed'));
     beforeEach(inject(function(_$rootScope_, _$compile_, _$httpBackend_) {
+        model = {};
         $rootScope = _$rootScope_;
         $httpBackend = _$httpBackend_;
         scope = $rootScope.$new();
         $compile = _$compile_;
-        element = $compile('<ng-embed></ng-embed>')($rootScope);
+        element = $compile('<ng-embed ng-model="model"></ng-embed>')(scope);
         $rootScope.$digest();
     }));
 
 
 
     describe('init', function() {
-        it('should respond an embed url', function() {
-            expect(element.isolateScope().embedUrl).toEqual('');
-//            $httpBackend.expectJSONP('').respond(200, {});
-            element = $compile('<ng-embed embed-url="https://www.youtube.com/watch?v=TWfph3iNC-k"></ng-embed>')($rootScope);
+        it('should create the right model', function() {
+            scope.model = {};
+            element = $compile('<ng-embed ng-model="model" embed-url="https://www.youtube.com/watch?v=TWfph3iNC-k"></ng-embed>')(scope);
             element.isolateScope().$digest();
-//            $httpBackend.flush();
-            expect(element.isolateScope().embedUrl).toEqual('https://www.youtube.com/watch?v=TWfph3iNC-k');
+            expect(scope.model.originalUrl).toBeTruthy();
+            expect(scope.model.longUrl).toBeTruthy();
+            expect(scope.model.provider).toBeTruthy();
+            expect(scope.model.html).toBeTruthy();
         });
     });
 
