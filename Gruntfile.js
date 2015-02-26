f5e01e13d705285bdd1256c5c75955b1a887b544
suggim01 <mike.suggitt@bbc.co.uk>
2015-02-26 10:17:01 +0000

    - Added minification to a dist directory

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,16 +1,30 @@
 module.exports = function (grunt) {
     'use strict';
     grunt.initConfig({
+        pkg: grunt.file.readJSON('package.json'),
         concat: {
-            admin: {
+            dist: {
                 src: 'app/**/*.js',
-                dest: 'ngEmbed.build.js'
+                dest: 'dist/ngEmbed.build.js'
+            }
+        },
+        uglify: {
+            options: {
+                // the banner is inserted at the top of the output
+                banner: '/*! <%= pkg.name %> <%= grunt.template.today("dd-mm-yyyy") %> */\n'
+            },
+            dist: {
+                files: {
+                    'dist/<%= pkg.name %>.min.js': ['<%= concat.dist.dest %>']
+                }
             }
         }});
 
     grunt.registerTask('build', [
-        'concat'
+        'concat',
+        'uglify'
     ]);
 
     grunt.loadNpmTasks('grunt-contrib-concat');
+    grunt.loadNpmTasks('grunt-contrib-uglify');
 };
